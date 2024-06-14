cabb8afdae41e371a9efd67f5eb228a4b625272c
CodeYouness <ylijassi@gmail.com>
2024-06-14 12:26:16 +0200

    added axios and function to call api

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,10 +1,33 @@
 <script>
+import { store } from "../store";
+import axios from 'axios';
+
 export default {
     data() {
         return {
+            store
         }
     },
-}
+    components: {
+        store
+    },
+    methods: {
+        getMovieList(){
+            axios.get('https://api.themoviedb.org/3/search/movie?api_key=aea95f4298835f7d0f3d896a8e981b61&query=ritorno+al+futuro')
+            .then((response) => {
+                // handle success
+                console.log(response.data.results);
+                this.store.cardList = response
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+        },
+    },
+    created(){
+        this.getMovieList()
+    }}
 </script>
 
 <template>
